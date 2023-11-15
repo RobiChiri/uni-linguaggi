e0a1e51a3c5485f11d27afa4ac0080cdd51a0356
Robichiri <robertochiriac8@gmail.com>
2023-11-15 18:29:38 +0100

    a

--- java/readFile/CheckInventory.java
+++ java/readFile/CheckInventory.java
@@ -1,47 +1,39 @@
 package readFile;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class CheckInventory {
-    public static void main(String[] args) {
-        String line, name;
-        int units, count = 0;
-        float price;
-        String file = "inventory.dat";
+        public static void ain(String[] ar  
+                int 
+                float price;
+                Stri
+          
 
-        InventoryItem[] items = new InventoryItem[100];
-        StringTokenizer tokenizer;
-
-        try {
-            FileReader fr = new FileReader(file);
-            BufferedReader inFile = new BufferedReader(fr);
-            line = inFile.readLine();
-            while (line != null) {
-                tokenizer = new StringTokenizer(line);
-                name = tokenizer.nextToken();
                 try {
-                    units = Integer.parseInt(tokenizer.nextToken());
-                    price = Float.parseFloat(tokenizer.nextToken());
-                    items[count++] = new InventoryItem(name, units, price);
-                } catch (NumberFormatException e) {
-                    System.out.println("Error in input. Line:" + line);
-                }
-                line = inFile.readLine();
-            }
+             
+            
+                            name 
 
-            inFile.close();
+                                 
 
-            // Scrive a video i dati letti
-            for (int scan = 0; scan < count; scan++)
-                System.out.println(items[scan]);
-        }
-        // Gestione delle eccezioni in cascata
+                     
+                                        S
+                                    }
+                                }  
+                    
+                              // Scrive a video i dati le
+                t
+            t
+                       
 
-        catch (FileNotFoundException e) {
-            System.out.println("File " + file + " not found.");
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
-}
+                    }
+                    // Ge s tione d e lle eccezioni 
+                
+         
+                    System.out.println("File "
+
+            } }
+            
+        
+    
