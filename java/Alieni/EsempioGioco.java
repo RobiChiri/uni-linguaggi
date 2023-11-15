ca7258bb712f322f79d48bbd4cce6ff1cd2a2000
Robichiri <robertochiriac8@gmail.com>
2023-11-15 18:04:36 +0100

    commit

--- java/Alieni/EsempioGioco.java
+++ java/Alieni/EsempioGioco.java
@@ -3,9 +3,10 @@ package Alieni;
 public class EsempioGioco {
   public static void main(String[] args) {
     GruppoAlieni ga = new GruppoAlieni(3);
-    ga.aggiungiAlieno(new AlienoSerpente(100, "Sirbiss"), 0);
-    ga.aggiungiAlieno(new AlienoOrco(100, "Shrek"), 1);
-    ga.aggiungiAlieno(new AlienoUomoMarshmallow(100, "Mork"), 2);
+    ga.aggiungiAlieno(new AlienoSerpente(100, "a"), 0);
+    ga.aggiungiAlieno(new AlienoOrco(100, "b"), 1);
+    ga.aggiungiAlieno(new AlienoUomoMarshmallow(100, "c"), 2);
+
     System.out.println("Danno: " + ga.calcolaDanno());
   }
 }
--- java/Alieni/GruppoAlieni.java
+++ java/Alieni/GruppoAlieni.java
@@ -1,37 +1,36 @@
 package Alieni;
 
 // public class GruppoAlieni {
-//   private Alieno[] alieno;
+// private Alieno[] alieno;
 
-//   public GruppoAlieni
+// public GruppoAlieni
 
-//   (int alieniNum) {
-//     alieno = new Alieno[alieniNum];
-//   }
-
-//   public void aggiungiAlieno(Alieno nuovoAlieno, int indice) {
-//     alieno[indice] = nuovoAlieno;
-//   }
-
-//   public Alieno[] getAlieni() {
-//     return alieno;
-//   }
-
-//   public int calcolaDanno() {
-//     int danno = 0;
-//     for (int i = 0; i < alieno.length; i++) {
-//       if (alieno[i].tipo == Alieno.ALIENO_SERPENTE) {
-//         danno += 10; // Il serpente procura un danno 10
-//       } else if (alieno[i].tipo == Alieno.ALIENO_ORCO) {
-//         danno += 6; // L’orco procura un danno 6
-//       } else if (alieno[i].tipo == Alieno.ALIENO_UOMO_MARSHMALLOW) {
-//         danno += 1; // L’Uomo Marshmallow procura un danno 1
-//       }
-//     }
-//     return danno;
-//   }
+// (int alieniNum) {
+// alieno = new Alieno[alieniNum];
 // }
 
+// public void aggiungiAlieno(Alieno nuovoAlieno, int indice) {
+// alieno[indice] = nuovoAlieno;
+// }
+
+// public Alieno[] getAlieni() {
+// return alieno;
+// }
+
+// public int calcolaDanno() {
+// int danno = 0;
+// for (int i = 0; i < alieno.length; i++) {
+// if (alieno[i].tipo == Alieno.ALIENO_SERPENTE) {
+// danno += 10; // Il serpente procura un danno 10
+// } else if (alieno[i].tipo == Alieno.ALIENO_ORCO) {
+// danno += 6; // L’orco procura un danno 6
+// } else if (alieno[i].tipo == Alieno.ALIENO_UOMO_MARSHMALLOW) {
+// danno += 1; // L’Uomo Marshmallow procura un danno 1
+// }
+// }
+// return danno;
+// }
+// }
 public class GruppoAlieni {
   private Alieno[] alieno;
 
@@ -43,15 +42,14 @@ public class GruppoAlieni {
     alieno[indice] = nuovAlieno;
   }
 
-  public Alieno[] getAlieno() {
+  public Alieno[] getAlieni() {
     return alieno;
   }
 
   public int calcolaDanno() {
     int danno = 0;
-    for (int i = 0; i < alieno.length; i++) {
+    for (int i = 0; i < alieno.length; i++)
       danno += alieno[i].getDanno();
-    }
 
     return danno;
   }
